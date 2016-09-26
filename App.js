186b51b7668c2c1a7db9e7369323436924404dce
Augustin <Augustinraj@users.noreply.github.com>
2016-09-26 12:13:23 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 function getmax() {
+    //Declaring NUmbers using Array//
     var numbers = [11, 22, 99],
+        //max funtion which returns the maximum value in an array//
         max = Math.max.apply(null,numbers);
+    //Displaying the largest number in console//
   console.log(max);
 }
 getmax(); 
